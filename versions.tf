06561a6dc3117a226bcaf3a95a574d147bbc9c06
Daniel Putzer <danielputzer.work@gmail.com>
2022-12-07 17:01:14 +0100

    chore: Update terraform cloud config

--- versions.tf
+++ versions.tf
@@ -1,4 +1,12 @@
 terraform {
+  cloud {
+     organization = "Pluralith"
+
+     workspaces {
+       name = "tfc-guide-example"
+     }
+  }
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
